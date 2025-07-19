6835c89b593fc9f3f2238ca038f8e5c53aa39d53
durgeshkushwaha <durgeshkushwaha361000@gmail.com>
2025-07-20 00:17:06 +0530

    added cout

--- Basic/Armstrong_number.cpp
+++ Basic/Armstrong_number.cpp
@@ -4,7 +4,7 @@ using namespace std;
 bool armstrongNumber(int n)
 {
     int sum = 0, m = n;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= 3; i++)
     {
         int x = m % 10;
         m /= 10;
@@ -16,6 +16,7 @@ int main()
 {
     int n;
     cout << "enter the value of n\n";
-    armstrongNumber(n);
+    cin >> n;
+    armstrongNumber(n) ? cout << "This is Armstrong Number" : cout << "This is not Armstrong Number";
     return 0;
 }
