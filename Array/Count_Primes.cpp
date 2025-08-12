076b5b2e87aa8e0bf9d96abcfe618fede6b1e8c3
durgeshkushwaha <durgeshkushwaha361000@gmail.com>
2025-08-13 00:02:39 +0530

    Count Primes

---
+++ Array/Count_Primes.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int countPrimes(int n)
+{
+    if (n <= 2)
+        return 0;
+
+    vector<bool> isPrime(n, true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i < n; i++)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j < n; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    int count = 0;
+    for (int i = 2; i < n; i++)
+    {
+        if (isPrime[i])
+            count++;
+    }
+
+    return count;
+};
+int main()
+{
+    int n = 10;
+    int ans = countPrimes(n);
+    cout << "the number of primes are -> " << ans;
+    return 0;
+}
